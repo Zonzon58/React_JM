8f4af62a578a052caeac0bf61a43dd902252ed21
Zonghero Jean Marc <jm.zonghero@gmail.com>
2023-11-06 21:48:21 +0100

    utilisation des useState quêtes 5 et 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,61 @@
-/*import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'*/
+import { useState } from "react";
+import Pokemoncard from "pokemoncard"
+import "./App.css";
 
-import PokemonCard from './components/PokemonCard'
-import './App.css'
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const previous = () => {
+    if (pokemonIndex > 0) {
+      console.log("Previous");
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const next = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      console.log("next");
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
 
   return (
     <div>
-      <PokemonCard />
+      {console.log(pokemonIndex)}
+      <nav>
+        <button onClick={() => previous()}>Précedent</button>
+        <button onClick={() => next()}>Suivant</button>
+      </nav>
+
+      <Pokemoncard pokemon={pokemonList[pokemonIndex]} />
     </div>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
